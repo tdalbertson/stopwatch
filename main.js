8a617346cae9aad65c186447af7cae5503ca7a78
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2024-07-08 23:08:24 -0700

    Resolve issue where seconds would not increment correctly

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ const startButton = document.querySelector(".start");
 const stopButton = document.querySelector(".stop");
 const resetButton = document.querySelector(".reset");
 
-// Time Text
-const oneHundredthText = document.querySelector("#one-hundredth");
-const secondText = document.querySelector("#second");
-
 // Time Values
 let oneHundredthSecond = 0;
 let second = 0;
@@ -41,37 +37,38 @@ document.addEventListener("counting", () => {
     countingInterval = setInterval(startCounting, 10);
 });
 
+document.addEventListener("countSecond", () => {
+    const secondValue = document.querySelector("#second");
+
+    if(second == 60) {
+        second = 0;
+    }
+
+    checkTimeLessThanTen(second, secondValue);
+    second++;
+});
+
 // Counting
 function startCounting() {
+    const oneHundredthText = document.querySelector("#one-hundredth");
+
     if(oneHundredthSecond == 100) {
         oneHundredthSecond = 0;
-        secondText.dispatchEvent(countSecondEvent);
+        document.dispatchEvent(countSecondEvent);
     }
 
     checkTimeLessThanTen(oneHundredthSecond, oneHundredthText);
-
     oneHundredthSecond++;
 }
 
-// Need to fix 'Sec' text disappearing when updating the seconds amount
-secondText.addEventListener("countSecond", () => {
-    console.log(second);
-    if(second == 60) {
-        second = 0;
-        console.log('Minute counted')
-    }
-
-    checkTimeLessThanTen(second, secondText);
-
-    second++;
-
-});
-
-
 function checkTimeLessThanTen(time, timeText) {
+    let newText = '';
+    
     if(time < 10) {
-        timeText.innerText = '0' + time;
+        newText = '0' + time;
     } else {
-        timeText.innerText = time;
+        newText = time;
     }
+
+    timeText.innerText = newText;
 }
