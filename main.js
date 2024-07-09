5174cf744d64c18523e939e9d049497c1b0519a5
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2024-07-08 23:06:09 -0700

    Change "...-text" spans to be siblings instead of children
    This fixes the issue with text shifting when seconds increment.

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-let counting = false;
+// Buttons
 const startButton = document.querySelector(".start");
 const stopButton = document.querySelector(".stop");
 const resetButton = document.querySelector(".reset");
-const oneHundredth = document.querySelector("#one-hundredth");
 
-const countingEvent = new CustomEvent("counting", {
-    detail: {
-        counting: true,
-    },
-});
+// Time Text
+const oneHundredthText = document.querySelector("#one-hundredth");
+const secondText = document.querySelector("#second");
 
+// Time Values
+let oneHundredthSecond = 0;
+let second = 0;
+
+// Time Events
+const countingEvent = new Event("counting");
+const countSecondEvent = new Event("countSecond");
+
+// Button Event Listeners
 startButton.addEventListener("click", () => {
     startButton.classList.add("disabled");
     stopButton.classList.remove("disabled");
@@ -28,17 +34,44 @@ stopButton.addEventListener("click", (event) => {
     clearInterval(countingInterval);
 });
 
+// Counting event listeners
 document.addEventListener("counting", () => {
     startButton.disabled = true;
 
     countingInterval = setInterval(startCounting, 10);
 });
 
+// Counting
 function startCounting() {
-    console.log("In setInterval");
+    if(oneHundredthSecond == 100) {
+        oneHundredthSecond = 0;
+        secondText.dispatchEvent(countSecondEvent);
+    }
 
-    oneHundredth.innerText++;
-    if (oneHundredth.innerText < 10) {
-        oneHundredth.innerText = `0${oneHundredth.innerText}`;
+    checkTimeLessThanTen(oneHundredthSecond, oneHundredthText);
+
+    oneHundredthSecond++;
+}
+
+// Need to fix 'Sec' text disappearing when updating the seconds amount
+secondText.addEventListener("countSecond", () => {
+    console.log(second);
+    if(second == 60) {
+        second = 0;
+        console.log('Minute counted')
+    }
+
+    checkTimeLessThanTen(second, secondText);
+
+    second++;
+
+});
+
+
+function checkTimeLessThanTen(time, timeText) {
+    if(time < 10) {
+        timeText.innerText = '0' + time;
+    } else {
+        timeText.innerText = time;
     }
 }
